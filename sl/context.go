ae14b506747039b57fd0ffe42cbda21d896362c9
Nikita <ns_stepanov@inbox.ru>
2024-11-19 00:31:26 +0300

    Update loggerFromContext with ctx

--- sl/context.go
+++ sl/context.go
@@ -3,6 +3,8 @@ package sl
 import (
 	"context"
 	"log/slog"
+
+	cctx "github.com/nikitaSstepanov/tools/ctx"
 )
 
 type ctxLogger struct{}
@@ -12,8 +14,11 @@ func ContextWithLogger(ctx context.Context, l *Logger) context.Context {
 	return context.WithValue(ctx, ctxLogger{}, l)
 }
 
-
 func loggerFromContext(ctx context.Context) *Logger {
+	if c, ok := ctx.(cctx.Context); ok {
+		return c.Logger()
+	}
+
 	if l, ok := ctx.Value(ctxLogger{}).(*Logger); ok {
 		return l
 	}
