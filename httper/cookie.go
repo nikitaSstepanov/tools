6250c55984b0c2b9e94540e457ffe5c435bd2a51
Nikita <ns_stepanov@inbox.ru>
2024-12-29 23:54:11 +0300

    Rename CookieCfg Path struct tag

--- httper/cookie.go
+++ httper/cookie.go
@@ -3,7 +3,7 @@ package httper
 type CookieCfg struct {
 	Name     string `yaml:"name"`
 	Age      int    `yaml:"age"`
-	Path     string `yaml:"string"`
+	Path     string `yaml:"path"`
 	Host     string `yaml:"host"`
 	Secure   bool   `yaml:"secure"`
 	HttpOnly bool   `yaml:"http_only"`
