6ad581194cbe68622515552d5c8e9f3ddd6426dd
Nikita <ns_stepanov@inbox.ru>
2025-01-01 21:54:47 +0300

    Add Pool interface to client/pg

--- client/pg/alias.go
+++ client/pg/alias.go
@@ -1,10 +1,25 @@
 package pg
 
 import (
+	"context"
+
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type Row = pgx.Row
+type (
+	Row            = pgx.Row
+	Rows           = pgx.Rows
+	Conn           = pgx.Conn
+	Tx             = pgx.Tx
+	TxOptions      = pgx.TxOptions
+	Stat           = pgxpool.Stat
+	Batch          = pgx.Batch
+	BatchResults   = pgx.BatchResults
+	Identifier     = pgx.Identifier
+	CopyFromSource = pgx.CopyFromSource
+)
 
 var (
 	ErrTxCommitRollback = pgx.ErrTxCommitRollback
@@ -12,3 +27,21 @@ var (
 	ErrTxClosed         = pgx.ErrTxClosed
 	ErrNoRows           = pgx.ErrNoRows
 )
+
+type Pool interface {
+	Acquire(ctx context.Context) (c *pgxpool.Conn, err error)
+	AcquireFunc(ctx context.Context, f func(*pgxpool.Conn) error) error
+	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
+	AcquireAllIdle(ctx context.Context) []*pgxpool.Conn
+	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
+	SendBatch(ctx context.Context, b *pgx.Batch) pgx.BatchResults
+	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
+	BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error)
+	CopyFrom(ctx context.Context, tableName pgx.Identifier, columnNames []string, rowSrc pgx.CopyFromSource) (int64, error)
+	Ping(ctx context.Context) error
+	Begin(ctx context.Context) (pgx.Tx, error)
+	Stat() *pgxpool.Stat
+	Config() *pgxpool.Config
+	Reset()
+	Close()
+}
--- client/pg/postregsql.go
+++ client/pg/postregsql.go
@@ -5,71 +5,16 @@ import (
 	"fmt"
 
 	"github.com/jackc/pgx/v5"
-	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 // Client is interface for communicate with postgres database.
 type Client interface {
-	// Begin starts a pseudo nested transaction implemented with a savepoint.
-	Begin(ctx context.Context) (pgx.Tx, error)
-
-	// BeginTx acquires a connection from the Pool and starts a transaction with pgx.TxOptions determining the transaction mode.
-	// Unlike database/sql, the context only affects the begin command. i.e. there is no auto-rollback on context cancellation.
-	// *pgxpool.Tx is returned, which implements the pgx.Tx interface.
-	// Commit or Rollback must be called on the returned transaction to finalize the transaction block.
-	BeginTx(ctx context.Context, txOptions pgx.TxOptions) (pgx.Tx, error)
-
-	// Close closes all connections in the pool and rejects future Acquire calls. Blocks until all connections are returned
-	// to pool and closed.
-	Close()
-
-	// Exec acquires a connection from the Pool and executes the given SQL.
-	// SQL can be either a prepared statement name or an SQL string.
-	// Arguments should be referenced positionally from the SQL string as $1, $2, etc.
-	// The acquired connection is returned to the pool when the Exec function returns.
-	Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error)
-
-	// Ping acquires a connection from the Pool and executes an empty sql statement against it.
-	// If the sql returns without error, the database Ping is considered successful, otherwise, the error is returned.
-	Ping(ctx context.Context) error
-
-	// Query acquires a connection and executes a query that returns pgx.Rows.
-	// Arguments should be referenced positionally from the SQL string as $1, $2, etc.
-	// See pgx.Rows documentation to close the returned Rows and return the acquired connection to the Pool.
-	//
-	// If there is an error, the returned pgx.Rows will be returned in an error state.
-	// If preferred, ignore the error returned from Query and handle errors using the returned pgx.Rows.
-	//
-	// For extra control over how the query is executed, the types QuerySimpleProtocol, QueryResultFormats, and
-	// QueryResultFormatsByOID may be used as the first args to control exactly how the query is executed. This is rarely
-	// needed. See the documentation for those types for details.
-	Query(ctx context.Context, sql string, args ...any) (pgx.Rows, error)
-
-	// QueryRow acquires a connection and executes a query that is expected
-	// to return at most one row (pgx.Row). Errors are deferred until pgx.Row's
-	// Scan method is called. If the query selects no rows, pgx.Row's Scan will
-	// return ErrNoRows. Otherwise, pgx.Row's Scan scans the first selected row
-	// and discards the rest. The acquired connection is returned to the Pool when
-	// pgx.Row's Scan method is called.
-	//
-	// Arguments should be referenced positionally from the SQL string as $1, $2, etc.
-	//
-	// For extra control over how the query is executed, the types QuerySimpleProtocol, QueryResultFormats, and
-	// QueryResultFormatsByOID may be used as the first args to control exactly how the query is executed. This is rarely
-	// needed. See the documentation for those types for details.
-	QueryRow(ctx context.Context, sql string, args ...any) pgx.Row
-
-	// Reset closes all connections, but leaves the pool open. It is intended for use when an error is detected that would
-	// disrupt all connections (such as a network interruption or a server state change).
-	//
-	// It is safe to reset a pool while connections are checked out. Those connections will be closed when they are returned
-	// to the pool.
-	Reset()
+	Pool
 
 	RegisterTypes(types []string) error
 
-	GetPool() *pgxpool.Pool
+	GetPool() Pool
 }
 
 // Config is type for database connection.
@@ -83,8 +28,8 @@ type Config struct {
 }
 
 type pgclient struct {
-	afterConnectFuncs []func(ctx context.Context, conn *pgx.Conn) error
-	*pgxpool.Pool
+	afterConnectFuncs []func(ctx context.Context, conn *Conn) error
+	Pool
 }
 
 // ConnectToDb returns a pointer to a pgxpool.Pool representing the database connection pool.
@@ -114,14 +59,14 @@ func New(ctx context.Context, cfg *Config) (Client, error) {
 	return client, nil
 }
 
-func NewWithPool(ctx context.Context, pool *pgxpool.Pool) (Client, error) {
+func NewWithPool(ctx context.Context, pool Pool) (Client, error) {
 	if err := pool.Ping(ctx); err != nil {
 		return nil, err
 	}
 
 	client := &pgclient{
-		Pool: pool,
-		afterConnectFuncs: make([]func(ctx context.Context, conn *pgx.Conn) error, 0),
+		Pool:              pool,
+		afterConnectFuncs: make([]func(ctx context.Context, conn *Conn) error, 0),
 	}
 
 	return client, nil
@@ -171,6 +116,6 @@ func (pc *pgclient) RegisterTypes(types []string) error {
 	return nil
 }
 
-func (pc *pgclient) GetPool() *pgxpool.Pool {
+func (pc *pgclient) GetPool() Pool {
 	return pc.Pool
 }
